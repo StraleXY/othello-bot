184f5255d29089a401a59bf53d1c5a3e7f6d7fcf
unknown <strahinja0123@gmail.com>
2023-04-17 12:17:50 +0200

    Add: Initial version of the game

---
+++ game.py
@@ -0,0 +1,107 @@
+EMPTY = '.'
+WHITE = 'W'
+BLACK = 'B'
+DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+
+class IllegalMoveException(Exception):
+    pass
+
+
+class Game:
+
+    def __init__(self, turn: str, rows: int = 8, cols: int = 8):
+        self.rows = rows
+        self.cols = cols
+        self.turn = turn  # Indicates who should play next 'B' or 'W'
+        self.board = self.__new_board(rows, cols)
+
+    def get_rows(self):
+        return self.rows
+
+    def get_cols(self):
+        return self.cols
+
+    def get_board(self):
+        return self.board
+
+    def get_turn(self):
+        return self.turn
+
+    def move(self, row: int, col: int):
+        sequences = self.is_valid_move(row, col, self.turn)
+        if not sequences:
+            return
+        self.__play_sequences(sequences)
+        if self.has_legal_moves(self.turn):
+            self.turn = self.opposite_turn(self.turn)
+
+    def is_game_over(self) -> bool:
+        return not self.has_legal_moves(BLACK) and not self.has_legal_moves(WHITE)
+
+    def return_winner(self) -> str:
+        black_score = self.__get_cells_count(BLACK)
+        white_score = self.__get_cells_count(WHITE)
+        return WHITE if white_score > black_score else BLACK if black_score > white_score else None
+
+    def print_board(self):
+        for row in self.board:
+            print(' '.join(row))
+        print('---------------')
+
+    # Private
+    @staticmethod
+    def __new_board(rows: int, cols: int) -> [[str]]:
+        _board = [[EMPTY for _ in range(cols)] for _ in range(rows)]
+        _board[rows // 2 - 1][cols // 2 - 1] = WHITE
+        _board[rows // 2 - 1][cols // 2] = BLACK
+        _board[rows // 2][cols // 2 - 1] = BLACK
+        _board[rows // 2][cols // 2] = WHITE
+        return _board
+
+    def __get_cells_count(self, turn: str) -> int:
+        return sum(1 for row in self.board for cell in row if cell == turn)
+
+    # TODO Make private
+    def has_legal_moves(self, turn) -> bool:
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.board[row][col] == EMPTY:
+                    if self.is_valid_move(row, col, self.opposite_turn(turn)):
+                        return True
+        return False
+
+    # TODO Make private
+    def is_valid_move(self, row, col, turn) -> [[(int, int)]]:
+        if self.board[row][col] != EMPTY:
+            return False
+
+        valid_sequences = []
+
+        for row_direction, col_direction in DIRECTIONS:
+            r, c = row + row_direction, col + col_direction
+            taken_pieces = []
+            sequence = [(row, col)]
+            while 0 <= r < self.rows and 0 <= c < self.cols:
+                if self.board[r][c] == self.opposite_turn(turn):
+                    taken_pieces.append(self.board[r][c])
+                    sequence.append((r, c))
+                    r += row_direction
+                    c += col_direction
+                elif self.board[r][c] == turn and taken_pieces:
+                    sequence.append((r, c))
+                    valid_sequences.append(sequence)
+                    break
+                else:
+                    break
+
+        return valid_sequences
+
+    def __play_sequences(self, sequences: [[(int, int)]]):
+        for sequence in sequences:
+            for cell in sequence:
+                self.board[cell[0]][cell[1]] = self.turn
+
+    @staticmethod
+    def opposite_turn(turn: str):
+        return WHITE if turn == BLACK else BLACK
--- main.py
+++ main.py
@@ -1,1 +1,80 @@
+from game import Game
 
+import tkinter as tk
+from game import Game
+
+class ReversiGUI(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.title("Othello")
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.canvas = tk.Canvas(self, width=400, height=400)
+        self.canvas.grid(row=0, column=0)
+        self.canvas.bind("<Button-1>", self.handle_click)
+
+        self.game = Game('W')
+        self.draw_board()
+
+    def draw_board(self):
+        self.canvas.delete("all")
+        for row in range(8):
+            for col in range(8):
+                x1 = col * 50
+                y1 = row * 50
+                x2 = x1 + 50
+                y2 = y1 + 50
+                if (row + col) % 2 == 0:
+                    self.canvas.create_rectangle(x1, y1, x2, y2, fill="green")
+                else:
+                    self.canvas.create_rectangle(x1, y1, x2, y2, fill="dark green")
+
+                if self.game.board[row][col] == 'B':
+                    self.canvas.create_oval(x1 + 5, y1 + 5, x2 - 5, y2 - 5, fill="black")
+                elif self.game.board[row][col] == 'W':
+                    self.canvas.create_oval(x1 + 5, y1 + 5, x2 - 5, y2 - 5, fill="white")
+
+    def handle_click(self, event):
+        col = event.x // 50
+        row = event.y // 50
+        if self.game.is_valid_move(row, col, self.game.turn):
+            self.game.move(row, col)
+            self.draw_board()
+
+            if self.game.is_game_over():
+                self.show_winner()
+            else:
+                if not self.game.has_legal_moves(self.game.turn):
+                    self.game.turn = self.game.opposite_turn(self.game.turn)
+                    if not self.game.has_legal_moves(self.game.turn):
+                        self.show_winner()
+
+    def show_winner(self):
+        black_count = sum(row.count('B') for row in self.game.board)
+        white_count = sum(row.count('W') for row in self.game.board)
+        if black_count > white_count:
+            winner = "Black"
+        elif white_count > black_count:
+            winner = "White"
+        else:
+            winner = "Tie"
+        self.canvas.create_text(200, 200, text=f"{winner} wins!", font=("Arial", 24))
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    game_gui = ReversiGUI(master=root)
+    game_gui.mainloop()
+
+
+# if __name__ == '__main__':
+#     game = Game('W')
+#     game.print_board()
+#     while True:
+#         print(game.get_turn())
+#         input_str = input("Enter move coordinates (row, col): ")
+#         x, y = map(int, input_str.split(","))
+#         game.move(x, y)
+#         game.print_board()
